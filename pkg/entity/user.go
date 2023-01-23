1c01722bfabe347cf93dbc1f12f657a6d2187fdf
SakataAtsuki <hhnkkr@icloud.com>
2023-01-23 22:41:13 +0900

    fix: user entity

--- pkg/entity/user.go
+++ pkg/entity/user.go
@@ -1,19 +1,15 @@
 package entity
 
+import "github.com/SakataAtsuki/e-architecture/pkg/proto/api"
+
 type User struct {
 	ID   string
 	Name string
-	// Gender    api.Gender
-	// UpdatedAt int64
 }
 
-// type Users []*User
-
-// func (u *User) Proto() (*api.User, error) {
-// 	return &api.User{
-// 		Id:        u.ID,
-// 		Name:      u.Name,
-// 		Gender:    u.Gender,
-// 		UpdatedAt: u.UpdatedAt,
-// 	}, nil
-// }
+func (u *User) Proto() *api.User {
+	return &api.User{
+		Id:   u.ID,
+		Name: u.Name,
+	}
+}
